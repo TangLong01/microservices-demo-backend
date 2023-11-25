4998ac2e3b826e5fc91e0905981d407686ec589c
Long <hlongoo450@gmail.com>
2023-11-25 14:21:40 +0700

    update port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const { Pool } = require("pg");
 const app = express();
-const port = 3001;
+const port = process.env.PORT || 3001;
 const cors = require("cors");
 const bodyParser = require("body-parser");
 
